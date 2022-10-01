adba847880a7178b95b2c9cd0acb504faac35ae2
Roman <sup.makulin@mail.ru>
2022-10-01 15:40:31 +0300

    Решили задачу на четность не четность

--- Examples_C#/task002/Program.cs
+++ Examples_C#/task002/Program.cs
@@ -1,24 +1,36 @@
 ﻿// Даны два числа. Показать большее и меньшее число
 
-int A = 11;
+int A = 10;
 int B = 10;
 int max = 0;
 int min = 0;
 
-if (A>B)
+if (A==B)
+{
+ Console.Write("У вас числа равны");
+
+}
+
+else if (A>B)
 {
     max = A;
     min = B;
-}
-
-if (A<B)
-{
-    max = B;
-    min = A;
-
-}
+    
 Console.Write("Макисмальное значение = ");
 Console.WriteLine(max);
 
 Console.Write("Минимальное значение = ");
 Console.WriteLine(min);
+}
+
+else if (A<B)
+{
+    max = B;
+    min = A;
+    
+Console.Write("Макисмальное значение = ");
+Console.WriteLine(max);
+
+Console.Write("Минимальное значение = ");
+Console.WriteLine(min);
+}
