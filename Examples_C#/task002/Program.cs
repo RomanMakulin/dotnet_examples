845b2dc5bf3a533e4aef771ed8d7dd32c98068e7
Roman <sup.makulin@mail.ru>
2022-10-01 15:03:56 +0300

    Решили вторую задачу

---
+++ Examples_C#/task002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int A = 11;
+int B = 10;
+int max = 0;
+int min = 0;
+
+if (A>B)
+{
+    max = A;
+    min = B;
+}
+
+if (A<B)
+{
+    max = B;
+    min = A;
+
+}
+Console.Write("Макисмальное значение = ");
+Console.WriteLine(max);
+
+Console.Write("Минимальное значение = ");
+Console.WriteLine(min);
