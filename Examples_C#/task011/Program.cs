685723ee09b06a8070f91b8fc9f3f1d6e4a25584
Roman <sup.makulin@mail.ru>
2022-10-04 12:56:42 +0300

    Порешали задачки в example

---
+++ Examples_C#/task011/Program.cs
@@ -0,0 +1,49 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+// Задать число с клавиатуры
+int Number(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// Расчетная часть
+int Math1(int number, int A)
+{
+    A = 7;
+    int result = number % A;
+
+    if (result == 0)
+    {
+        Console.WriteLine($"Число {A} кратно заданному");
+    }
+    else
+    {
+        Console.WriteLine($"Число {A} не кратно заданному");
+    }
+
+    return result;
+}
+
+int Math2(int number, int B)
+{
+    B = 23;
+    int result = number % B;
+
+    if (result == 0)
+    {
+        Console.WriteLine($"Число {B} кратно заданному");
+    }
+    else
+    {
+        Console.WriteLine($"Число {B} не кратно заданному");
+    }
+
+    return result;
+}
+int A = 7;
+int B = 23;
+int number = Number("Введите число = ");
+Math1(number, A);
+Math2(number, B);
---
+++ Examples_C#/task012/Program.cs
@@ -0,0 +1,35 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+// Задаем 2 числа 
+int input(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+// Проверяем a = b^2?
+int Math(int a, int b)
+{
+// проверяем первое число
+    if (a == b*b)
+    {
+        Console.WriteLine($"Число {a} является квадратом {b}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {a} не является квадратом {b}");
+    }
+// проверяем второе число
+        if (b == a*a)
+    {
+        Console.WriteLine($"Число {b} является квадратом {a}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {b} не является квадратом {a}");
+    }
+    return a;
+}
+int a = input("Введите число A = ");
+int b = input("Введите число B = ");
+Math(a, b);
