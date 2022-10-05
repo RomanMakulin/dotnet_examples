549e25fc0be31dc7dac45ed7cec9a7fd027f2a0a
Roman <sup.makulin@mail.ru>
2022-10-05 11:56:25 +0300

    25. Найти сумму чисел от 1 до А

---
+++ Examples_C#/task016/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("25. Найти сумму чисел от 1 до А");
+int Input(string text)
+
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Sum(int number)
+{
+    int index = 0;
+    int result = 0;
+    while (index <= number)
+    {
+        result += index;
+        index++;
+    }
+Console.Write($"Сумма элементов числа {number} = {result} ");
+    return number;
+}
+
+int number = Input("Введите число = ");
+Sum(number);
