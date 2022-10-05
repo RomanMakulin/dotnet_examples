84d97722869299af4986405f197e8b348ffc483a
Roman <sup.makulin@mail.ru>
2022-10-05 09:04:47 +0300

    вывод корней от 1 до N

---
+++ Examples_C#/task015/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Найти кубы чисел от 1 до N");
+// Ввод числа N
+int Input(string text)
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+// Расчетная часть
+int Math(int number)
+{
+    while (1 <= number)
+    {
+    int numberSqr = number * number;
+    number--;
+    Console.WriteLine($"Корень из числа {number+1} = {numberSqr}");       
+    }
+return number;
+}
+// Код
+int number = Input("Введите число = ");
+Math(number);
