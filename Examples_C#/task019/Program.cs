63429d524245bc5eb52bc0f4a97f0659d05abe3c
Roman <sup.makulin@mail.ru>
2022-10-05 15:07:28 +0300

    Подсчитать сумму цифр в числе

---
+++ Examples_C#/task019/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Подсчитать сумму цифр в числе");
+
+int Input(string text)
+
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Sum(int number)
+{
+    int index = 0;
+    int result = 0;
+    while (index <= number)
+    {
+        result += index;
+        index++;
+    }
+Console.Write($"Сумма элементов числа {number} = {result} ");
+    return number;
+}
+
+int number = Input("Введите число = ");
+Sum(number);
