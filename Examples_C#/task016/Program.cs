70eee9887dc9da70f7a5b99cd0296a1d26f92d94
Roman <sup.makulin@mail.ru>
2022-10-05 15:08:07 +0300

    25. Найти сумму чисел от 1 до А.

--- Examples_C#/task016/Program.cs
+++ Examples_C#/task016/Program.cs
@@ -1,24 +1,46 @@
-﻿Console.WriteLine("25. Найти сумму чисел от 1 до А");
-int Input(string text)
+﻿Console.WriteLine("25. Найти сумму чисел от 1 до А.");
 
+void FillArray(int[]collection) // Заполнили массив
 {
-    Console.Write(text);
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
-}
-
-int Sum(int number)
-{
+    int count = collection.Length;
     int index = 0;
-    int result = 0;
-    while (index <= number)
+    while (index < count)
     {
-        result += index;
+        collection[index] = new Random().Next(1,4);
         index++;
     }
-Console.Write($"Сумма элементов числа {number} = {result} ");
-    return number;
 }
 
-int number = Input("Введите число = ");
-Sum(number);
+void PrintArray(int[] collection) // Печатаем массив
+{
+    int count = collection.Length;
+    int index = 0;
+    Console.Write("Массив: ");
+    while (index < count)
+    {
+        Console.Write("  "+ collection[index]);
+    index++;
+    }
+}
+
+int Math(int[] collection, int sum)
+{
+    int index = 0;
+    sum = 0;
+    int count = collection.Length;
+
+    while (index < count)
+    {
+        sum += collection[index];
+        index++;
+    }
+    return sum;
+}
+int sum = 0;
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int sum1 = Math(array, sum);
+Console.WriteLine($"Сумма элементов массива = {sum1}");
