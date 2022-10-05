7fc9da0bdebafc169b93756e902f9eb3c0f7c7d9
Roman <sup.makulin@mail.ru>
2022-10-05 12:43:55 +0300

    26. Возведите число А в натуральную степень B используя цикл

---
+++ Examples_C#/task017/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("26. Возведите число А в натуральную степень B используя цикл");
+
+int input(string text)// Вводим число в консоль с клавиатуры
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Method(int A, int B)
+{
+    int AStr = Convert.ToInt32(Math.Pow(A, B));
+    Console.WriteLine($"Число {A} в степени {B} = {AStr}");
+    return AStr;
+}
+
+int A = input("Введите число A = ");
+int B = input("Введите число B = ");
+Method(A, B);
