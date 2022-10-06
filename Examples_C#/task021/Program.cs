1d3d82d6bd367cea4583bcf7b27ee99c90a0a236
Roman <sup.makulin@mail.ru>
2022-10-06 17:44:14 +0300

    начал решать задачу на куб массива

---
+++ Examples_C#/task021/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Показать кубы чисел, заканчивающихся на четную цифру");
+void FillArray(int[] collection)
+{
+    int index = 0;
+    int count = collection.Length;
+    while (index < count)
+    {
+        collection[index] = new Random().Next(0,4);
+        index++;
+    }
+}
+void PrintArray(int[] collection)
+{
+    int index = 0;
+    int count = collection.Length;
+    Console.Write("Массив чисел = ");
+    while (index < count)
+    {
+        Console.WriteLine("  " + collection[index]);
+        index ++;
+    }
+}
+
+int Math(int[] collection, int number)
+{
+    int index = 0;
+    int count = collection.Length;
+    int stephen = 3;
+    int A = collection[index];
+
+    while (index < count)
+    {
+        number = Convert.ToInt32(Math.Pow(A, stephen));
+        if (number % 2 == 0)
+        {
+            
+            Console.WriteLine($"{number} - четное число");
+        }
+        index++;
+    }
+    return number;
+}
+
+int number = 1;
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int number1 = Math(array, number);
