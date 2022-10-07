a9e38ddddfe6482cb81ed22f990e8f2a84660642
Roman <sup.makulin@mail.ru>
2022-10-07 15:42:45 +0300

    Порешал задачки

---
+++ Examples_C#/task026/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных/четных чисел");
+//36. Задать массив, заполнить случайными положительными трёхзначными числами. 
+//Показать количество нечетных\четных чисел
+
+void FillArray(int[] collection)
+{
+    int index = 0;
+    int count = collection.Length;
+    while (index < count)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+void PrintArray(int[] collection)
+{
+    int index = 0;
+    int count = collection.Length;
+    while (index < count)
+    {
+        Console.Write("  " + collection[index]);
+        index++;
+    }
+}
+int KolVo(int[] collection, int chet, int nechet)
+{
+    int index = 0;
+    int count = collection.Length;
+    while (index < count)
+    {
+        if (collection[index] % 2 == 0)
+        {
+            chet += index;
+            index++;
+        }
+
+        else
+        {
+            nechet += index;
+            index++;
+        }
+        
+    }
+    Console.WriteLine($"Количество четных элементов: {chet} ");
+    Console.WriteLine($"Количество не четных элементов: {nechet} ");
+    return chet;
+}
+
+int chet = 0;
+int nechet = 0;
+int[] array = new int[3];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+KolVo(array, chet, nechet);
