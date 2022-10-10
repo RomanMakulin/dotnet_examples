5bea617c2a31722e991d4a24c4cf89718c8958be
Roman <sup.makulin@mail.ru>
2022-10-10 12:46:59 +0300

    решил 3 задачу третьего семинара

---
+++ HomeWork/3_sem/task003/Program.cs
@@ -0,0 +1,26 @@
+﻿// Home Work. 3 sem. 3 example.
+// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+int input(string text)
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Maths(string text, int number)
+{
+    for (int index = 1; index < number; number--)
+    {
+        int result = Convert.ToInt32(Math.Pow(number, 3));
+
+        Console.Write(number);
+        Console.WriteLine(text + result);
+    }
+    return number;
+}
+
+int number = input("Введите число: ");
+Console.WriteLine("Таблица кубов от 1 до N");
+Maths(" в кубе = ", number);
