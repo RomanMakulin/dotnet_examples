99bb4dc20703fadd956743e247be9d4e2f830a9e
Roman <sup.makulin@mail.ru>
2022-10-10 14:01:53 +0300

    решил 1 задачу четвертого семинара

---
+++ HomeWork/4_sem/task001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Home Work. 4 sem. 1 example.
+// Напишите программу, которая принимает на вход два числа
+// (А и В) и возводит число А в целую степень В. (!!Без MathPow!!)
+// 2 3
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int VvodStepen(string text, int number, int stepen, int result)
+{
+    int index = 0;
+    while (index < stepen)
+    {
+        result = result * number;
+        index++;
+    }
+    Console.WriteLine(text + result);
+    return number;
+}
+int result = 1;
+int number = InputNumber("Введите число A ");
+int stepen = InputNumber("Введите число B ");
+VvodStepen($" Число A ({number}) в степени B ({stepen}) = ", number, stepen, result);
