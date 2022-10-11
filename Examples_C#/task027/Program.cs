6a305205507a9ed818d4ab9fe4f8a6f1ae949fab
Roman <sup.makulin@mail.ru>
2022-10-11 20:43:28 +0300

    порешал

--- Examples_C#/task027/Program.cs
+++ Examples_C#/task027/Program.cs
@@ -2,13 +2,20 @@
 
 void FillArray(int[] collection)
 {
-    for (int index = 0; index < collection.Length; index++) collection[index] = new Random().Next(10, 100);
+    for (int index = 0; index < collection.Length; index++) collection[index] = new Random().Next(1, 10);
 }
 void PrintArray(int[]collection)
 {
     for (int index = 0; index < collection.Length; index++) Console.Write(" " + collection[index]);
 }
 
-int[] array = new int [123];
+int numbers(int[] collection, int result)
+{
+   int[] NewArray = new int{collection[index]}
+    
+}
+int result = 0;
+int[] array = new int [8];
 FillArray(array);
 PrintArray(array);
+numbers(array, result);
