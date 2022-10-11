53238df487ed47800885df1c5ef5e94a3ba2b28b
Roman <sup.makulin@mail.ru>
2022-10-11 16:36:53 +0300

    начал решать задачу 37

--- Examples_C#/task027/Program.cs
+++ Examples_C#/task027/Program.cs
@@ -1,5 +1,14 @@
-﻿/*
-Задача 18: Напишите программу, которая по заданному 
-номеру четверти, показывает диапазон возможных 
-координат точек в этой четверти (x и y).
-/*/
+﻿Console.WriteLine("37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]");
+
+void FillArray(int[] collection)
+{
+    for (int index = 0; index < collection.Length; index++) collection[index] = new Random().Next(10, 100);
+}
+void PrintArray(int[]collection)
+{
+    for (int index = 0; index < collection.Length; index++) Console.Write(" " + collection[index]);
+}
+
+int[] array = new int [123];
+FillArray(array);
+PrintArray(array);
