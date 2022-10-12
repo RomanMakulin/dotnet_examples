388dfc3fa02fd3e9133c794ac1b6bec5a5324a9c
Roman <sup.makulin@mail.ru>
2022-10-12 15:36:36 +0300

    решена задача на замену символов

--- Examples_C#/task031/Program.cs
+++ Examples_C#/task031/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.WriteLine("Провести тренировку по замене элементов текста в строке");
+
+string Change (string text, char oldtext, char newText)
+{
+    string result = String.Empty;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == oldtext) result = result + $"{newText}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string text = "Hello World.";
+
+string newText = Change(text, '.', '!');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Change(newText, ' ', '_');
+Console.WriteLine(newText);
