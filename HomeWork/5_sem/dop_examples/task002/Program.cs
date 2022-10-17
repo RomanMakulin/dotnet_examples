734c19e90d3f392f8606fa9d948f9532163d19da
Roman <sup.makulin@mail.ru>
2022-10-17 13:34:47 +0300

    Решил доп задачки с семинара

---
+++ HomeWork/5_sem/dop_examples/task002/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив. Напишите программу, которая 
+// определяет, присутствует ли заданное число в массиве.
+// Пример:
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        System.Console.Write($"Введите число под индексом {i} = ");
+        collection[i] = int.Parse(Console.ReadLine());
+    }
+}
+void PrintArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++) System.Console.Write($"{collection[i]} \t");
+}
+int FindNumber(int[] collection, int find)
+{
+    int result = -1;
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] == find) result = collection[i];
+    }
+    Console.WriteLine(result);
+    return find;
+}
+
+System.Console.Write("Введите искомое число: ");
+int find = int.Parse(Console.ReadLine());
+int[] array = new int[6];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.Write("Искомое число присутствует:  "); // Выдаст -1 если числа там нет
+FindNumber(array, find);
---
+++ HomeWork/5_sem/dop_examples/task003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++) collection[i] = Convert.ToInt32(new Random().Next(-100, 100));
+}
+void PrintArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++) System.Console.Write($"{collection[i]} \t");
+}
+void Switch(int[] collection)
+{
+    int[] NewCollection = new int[collection.Length];
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] > 0) System.Console.Write($"{NewCollection[i] = collection[i] * (-1)} \t");
+        else System.Console.Write($"{NewCollection[i] = collection[i] * (-1)} \t");
+    }
+    return;
+}
+
+int[] array = new int[6];
+FillArray(array);
+System.Console.Write("Массив: ");
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.Write("Смена знаков элементов массива на противположне: ");
+Switch(array);
