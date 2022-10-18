e8793231f0163e2f15b6a77840ce864c05585c11
Roman <sup.makulin@mail.ru>
2022-10-18 13:25:58 +0300

    порешал задачи на двумерный массив

---
+++ Examples_C#/task045/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("55. Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.");
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+double[] SrArifm(int[,] array)
+{
+    double[] OneArray = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        OneArray[j] = sum / array.GetLength(0);
+        System.Console.WriteLine($"{OneArray[j]} \t");
+    }
+    return OneArray;
+}
+
+int[,] array = new int[4, 6];
+FillArray(array);
+System.Console.WriteLine("Матрица: ");
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine("Среднее арифм столбцов: ");
+SrArifm(array);
---
+++ Examples_C#/task046/Program.cs
@@ -0,0 +1,48 @@
+﻿// 56. Написать программу, которая обменивает элементы первой строки и последней строки
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+void SumEl(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        var temp = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = array[0, i];
+        array[0, i] = temp;
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int[,] array = new int[5, 7];
+FillArray(array);
+System.Console.WriteLine("Массив: ");
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine("Поменял первую и последнюю строчку");
+SumEl(array);
