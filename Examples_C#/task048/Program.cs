01004b83208fce6e55ee5541022b1d594f36cb31
Roman <sup.makulin@mail.ru>
2022-10-21 15:44:51 +0300

    разбираюсь с матрицами

---
+++ Examples_C#/task048/Program.cs
@@ -0,0 +1,41 @@
+﻿// 58. Написать программу, которая в двумерном массиве 
+// заменяет строки на столбцы или сообщить, что это невозможно 
+// (в случае, если матрица не квадратная).
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(1, 10);
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) System.Console.Write($"{array[i, j]} \t");
+        System.Console.WriteLine();
+    }
+}
+void ChangeArr(int[,] array)
+{
+    if (array.GetLength(0) == array.GetLength(1))
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                System.Console.Write($"{array[j, i]} \t");
+            }
+            System.Console.WriteLine();
+        }
+    }
+    else System.Console.WriteLine("Матрица не квадратная ");
+}
+
+int[,] array = new int[4, 4];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine("Проведена замена строк на столбцы: ");
+ChangeArr(array);
---
+++ Examples_C#/task049/Program.cs
@@ -0,0 +1,38 @@
+﻿// 59. В прямоугольной матрице найти строку с наименьшей суммой элементов.
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(0, 3);
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) System.Console.Write($"{array[i, j]} \t");
+        System.Console.WriteLine();
+    }
+}
+void MaxSumString(int[,] array)
+{
+    int[] ArrayString = new int[array.GetLength(1)];
+    int min = ArrayString[0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        ArrayString[i] = sum;
+        }
+        for (int k = 0; k < ArrayString.Length; k++)
+    System.Console.WriteLine(sum);
+    }
+}
+
+int[,] array = new int[3, 4];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+MaxSumString(array);
