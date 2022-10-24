71ad06bc1db86a1868a1d78547748bb4a0d6a873
Roman <sup.makulin@mail.ru>
2022-10-24 11:19:41 +0300

    Решил 3 задачу ДЗ из 7 семинара

---
+++ HomeWork/8_sem/task003/Program.cs
@@ -0,0 +1,51 @@
+﻿// 3. Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(0, 99);
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) System.Console.Write($"{array[i, j]} \t");
+        System.Console.WriteLine();
+    }
+}
+void FilterArray(int[,] array)
+{
+    int max = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            max = array[i, j];
+            int index = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > max)
+                {
+                    max = array[i, k];
+                    index = k;
+                }
+            }
+            int temp = array[i, j];
+            array[i, j] = max;
+            array[i, index] = temp;
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = new int[4, 5];
+FillArray(array);
+System.Console.WriteLine("Дан двумерный массив: ");
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine("Массив отсортирован по убыванию: ");
+FilterArray(array);
