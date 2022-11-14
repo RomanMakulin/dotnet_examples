c516c233d85ad1dbf149a59d28139550579efcff
Roman <sup.makulin@mail.ru>
2022-11-14 16:00:58 +0300

    completed 9 sem homework

--- HomeWork/9_sem/task003/Program.cs
+++ HomeWork/9_sem/task003/Program.cs
@@ -8,13 +8,17 @@
 int[,,] array = new int[2, 2, 2];
 void FillArray(int[,,] array)
 {
+    int a = 10;
+    int b = 15;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                array[i, j, k] = new Random().Next(0, 10); ;
+                array[i, j, k] = new Random().Next(a, b);
+                a += 5;
+                b += 5;
             }
         }
     }
@@ -27,39 +31,11 @@ void PrintArray(int[,,] array)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                System.Console.Write($"{array[i, j, k]} \t");
+                System.Console.Write($"{array[i, j, k]}({i},{k},{j}) \t");
             }
             System.Console.WriteLine();
         }
-        System.Console.WriteLine();
     }
 }
-void DontRepeatArrayWithIndexElements(int[,,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                for (int m = k + 1; m < array.GetLength(2); m++)
-                {
-                    if (array[i, j, k] == array[i, j, m])
-                    {
-                        array[i, j, m] = new Random().Next(20, 30);
-                    }
-                }
-
-
-                System.Console.Write($"{array[i, j, k]} \t");
-            }
-            System.Console.WriteLine();
-        }
-        System.Console.WriteLine();
-    }
-}
-
 FillArray(array);
 PrintArray(array);
-System.Console.WriteLine("***");
-DontRepeatArrayWithIndexElements(array);
